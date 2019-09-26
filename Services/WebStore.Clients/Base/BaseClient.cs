195c912e9db829dcdbdd6125082c3a75dfd611f9
DmitrijBalalykin <vdv86@list.ru>
2019-09-26 21:06:37 +0300

    Clients for system Identity

--- Services/WebStore.Clients/Base/BaseClient.cs
+++ Services/WebStore.Clients/Base/BaseClient.cs
@@ -4,11 +4,12 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace WebStore.Clients.Base
 {
-    public abstract class BaseClient
+    public abstract class BaseClient : IDisposable
     {
         protected readonly HttpClient _Client;
 
@@ -32,9 +33,9 @@ namespace WebStore.Clients.Base
             headers.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         }
 
-        protected async Task<T> GetAsync<T>(string url) where T: new()
+        protected async Task<T> GetAsync<T>(string url, CancellationToken Cancel = default) where T: new()
         {
-            var response = await _Client.GetAsync(url);
+            var response = await _Client.GetAsync(url, Cancel);
             if (response.IsSuccessStatusCode)
                 return await response.Content.ReadAsAsync<T>();
 
@@ -43,7 +44,7 @@ namespace WebStore.Clients.Base
 
         protected T Get<T>(string url) where T : new() => GetAsync<T>(url).Result;
 
-        protected async Task<HttpResponseMessage> PostAsync<T>(string url,T item)
+        protected async Task<HttpResponseMessage> PostAsync<T>(string url,T item, CancellationToken Cancel = default)
         {
             var response = await _Client.PostAsJsonAsync(url, item);
             return response.EnsureSuccessStatusCode();
@@ -51,16 +52,22 @@ namespace WebStore.Clients.Base
 
         protected HttpResponseMessage Post<T>(string url, T item) => PostAsync(url, item).Result;
 
-        protected async Task<HttpResponseMessage> PutAsync<T>(string url, T item)
+        protected async Task<HttpResponseMessage> PutAsync<T>(string url, T item, CancellationToken Cancel = default)
         {
-            var responce = await _Client.PutAsJsonAsync(url, item);
+            var responce = await _Client.PutAsJsonAsync(url, item, Cancel);
             return responce.EnsureSuccessStatusCode();
         }
 
         protected HttpResponseMessage Put<T>(string url, T item) => PutAsync(url, item).Result;
 
-        protected async Task<HttpResponseMessage> DeleteAsync(string url) => await _Client.DeleteAsync(url);
+        protected async Task<HttpResponseMessage> DeleteAsync(string url, CancellationToken Cancel = default) => 
+            await _Client.DeleteAsync(url, Cancel);
 
         protected HttpResponseMessage Delete(string url) => DeleteAsync(url).Result;
+
+        public void Dispose()
+        {
+            _Client.Dispose();
+        }
     }
 }
---
+++ Services/WebStore.Clients/Roles/RolesClient.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using WebStore.Clients.Base;
+
+namespace WebStore.Clients.Roles
+{
+    public class RolesClient : BaseClient, IRoleStore<IdentityRole>
+    {
+        public RolesClient(IConfiguration configuration) : base(configuration, "api/roles")
+        {
+
+        }
+
+
+        public async Task<IdentityResult> CreateAsync(IdentityRole role, CancellationToken cancel)
+        {
+            return await (await PostAsync(_ServiceAddress, role, cancel))
+                .Content
+                .ReadAsAsync<bool>(cancel)
+                ? IdentityResult.Success
+                : IdentityResult.Failed();
+        }
+
+        public async Task<IdentityResult> DeleteAsync(IdentityRole role, CancellationToken cancel)
+        {
+            return await (await PostAsync($"{_ServiceAddress}/Delete", role, cancel))
+                .Content
+                .ReadAsAsync<bool>(cancel)
+                ? IdentityResult.Success
+                : IdentityResult.Failed();
+        }
+
+        public async Task<IdentityRole> FindByIdAsync(string id, CancellationToken cancel)
+        {
+            return await GetAsync<IdentityRole>($"{_ServiceAddress}/FindById/{id}", cancel);
+        }
+
+        public async Task<IdentityRole> FindByNameAsync(string name, CancellationToken cancel)
+        {
+            return await GetAsync<IdentityRole>($"{_ServiceAddress}/FindByName/{name}", cancel);
+        }
+
+        public async Task<string> GetNormalizedRoleNameAsync(IdentityRole role, CancellationToken cancel)
+        {
+            return await (await PostAsync($"{_ServiceAddress}/GetNormalizedRoleName", role, cancel))
+                .Content
+                .ReadAsAsync<string>(cancel);
+        }
+
+        public async Task<string> GetRoleIdAsync(IdentityRole role, CancellationToken cancel)
+        {
+            return await (await PostAsync($"{_ServiceAddress}/GetRoleId", role, cancel))
+                .Content
+                .ReadAsAsync<string>(cancel);
+        }
+
+        public async Task<string> GetRoleNameAsync(IdentityRole role, CancellationToken cancel)
+        {
+            return await (await PostAsync($"{_ServiceAddress}/GetRoleName", role, cancel))
+                .Content
+                .ReadAsAsync<string>(cancel);
+        }
+
+        public async Task SetNormalizedRoleNameAsync(IdentityRole role, string name, CancellationToken cancel)
+        {
+            role.NormalizedName = name;
+            await PostAsync($"{_ServiceAddress}/SetNormalizedRoleName/{name}", role, cancel);
+        }
+
+        public async Task SetRoleNameAsync(IdentityRole role, string name, CancellationToken cancel)
+        {
+            role.Name = name;
+            await PostAsync($"{_ServiceAddress}/SetRoleName/{name}", role, cancel);
+        }
+
+        public async Task<IdentityResult> UpdateAsync(IdentityRole role, CancellationToken cancel)
+        {
+            return await (await PutAsync(_ServiceAddress, role, cancel))
+                 .Content
+                 .ReadAsAsync<bool>(cancel)
+                 ? IdentityResult.Success
+                 : IdentityResult.Failed();
+        }
+    }
+}
