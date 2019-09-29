c33cb1d3ced8d220095aa2d48e3b60019657a309
DmitrijBalalykin <vdv86@list.ru>
2019-09-30 00:34:31 +0300

    Logger and Provider for working from Lo4Net

---
+++ Common/WebStore.Logger/Log4NetExtentions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace WebStore.Logger
+{
+    public static class Log4NetExtentions
+    {
+        public static ILoggerFactory AddLog4Net(this ILoggerFactory factory, 
+                                                string configurationFile = "log4net.config")
+        {
+            if (!Path.IsPathRooted(configurationFile))
+            {
+                var assemble = Assembly.GetEntryAssembly() ?? throw new InvalidOperationException("Не найдена сборка исполнительного файла");
+                var dir = Path.GetDirectoryName(assemble.Location) ?? throw new InvalidOperationException ("Не определена дирректория исполнительного файла");
+                configurationFile = Path.Combine(dir, configurationFile);
+
+            }
+
+            factory.AddProvider(new Log4NetLoggerProvider(configurationFile));
+            return factory;
+        }
+    }
+}
