d4d9f64cc35b37de34ddbe61483abdfca2b044a5
DmitrijBalalykin <vdv86@list.ru>
2019-10-07 10:10:54 +0300

    Site Map

---
+++ UI/WebStore/Controllers/SitemapController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SimpleMvcSitemap;
+using WebStore.DomainNew.Filters;
+using WebStore.Infrastucture.Interfaces;
+
+namespace WebStore.Controllers
+{
+    public class SitemapController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public SitemapController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public IActionResult Index()
+        {
+            var nodes = new List<SitemapNode>
+            {
+                new SitemapNode(Url.Action("Home", "Index")),
+                new SitemapNode(Url.Action("Product", "Shop")),
+            };
+
+            foreach (var section in _productService.GetSections())
+                if (section.ParentId != null)
+                    nodes.Add(new SitemapNode(Url.Action("Shop", "Product", new { SectionId = section.Id })));
+
+            foreach (var brand in _productService.GetBrands())
+                nodes.Add(new SitemapNode(Url.Action("Shop", "Product", new { BrandId = brand.Id })));
+
+            foreach (var product in _productService.GetProducts(new ProductFilter()))
+                nodes.Add(new SitemapNode(Url.Action("ProductDetails", "Product", new { product.Id })));
+
+            return new SitemapProvider().CreateSitemap(new SitemapModel(nodes));
+        }
+    }
+}
