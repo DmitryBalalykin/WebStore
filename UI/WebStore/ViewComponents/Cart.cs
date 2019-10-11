392ef005c1397e8c66f6fcb84591ab9ffb7197ab
DmitrijBalalykin <vdv86@list.ru>
2019-10-11 09:40:57 +0300

    Ассинхронная корзина

---
+++ UI/WebStore/ViewComponents/Cart.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Interface.Services;
+
+namespace WebStore.ViewComponents
+{
+    public class Cart : ViewComponent
+    {
+        private readonly ICartService _cartService;
+
+        public Cart(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+           return View(_cartService.TransformCart());
+        }
+    }
+}
